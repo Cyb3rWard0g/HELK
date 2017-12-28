6732ebe8ab150e2e968e1907f3d9327170176b4d
JP <potti.jordan@gmail.com>
2017-12-28 12:29:38 -0600

    Update cleanup.sh

--- curator/cleanup.sh
+++ curator/cleanup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#This bash script will delete the oldest logs and move to more recent logs until it reaches the desired free space on disk.
+#Running curator in this fashion is only recomended for single node clusters or standalone setup's such as the HELK. 
+# This df command grabs the free space of the root '/', if you store your logs elsewhere you will have to modify this.
 disk=$(df -H | grep -vE '^Mounted| /.' | awk '{ print $1 " " $5 " " $6 }' | awk 'NR == 2' | awk '{print $2}' |sed 's/%//')
-days=31
-while [ "$disk" > 95 ] && [ "$days" != 2 ]
+#If you have more than 90 days of logs, this number will have to be increased, went with 90 days with the idea that you will reach 
+#your disk space limit before 90 days. 
+days=90
+# Disk threshold at 80 percent and also will not delete logs within the last 2 days.
+while [ "$disk" > 80 ] && [ "$days" != 2 ]
 do
 daysreplace='unit_count: '$days
 echo $daysreplace
