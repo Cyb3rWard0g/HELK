8efbc767883d5bb077240f47569b4a90a82b8ab4
JP <potti.jordan@gmail.com>
2018-02-21 17:10:46 -0600

    Update helk_debian_tar_install.sh

--- scripts/helk_debian_tar_install.sh
+++ scripts/helk_debian_tar_install.sh
@@ -436,7 +436,6 @@ echo "[HELK-BASH-INSTALLATION-INFO] Creating a cronjob for curator"
 cp ../curator/* /opt/helk/curator
 cronjob="0 0 * * * /usr/local/bin/curator --config /opt/helk/curator/config.yml /opt/helk/curator/delete-after.yml"
 crontab -l | { cat; echo "$cronjob"; } | crontab - >> $LOGFILE 2>&1
-echo "$cronjob" | crontab - >> $LOGFILE 2>&1
 cronjob="0 * * * 0 /usr/local/bin/curator --config /opt/helk/curator/config.yml /opt/helk/curator/forcemerge.yml"
 crontab -l | { cat; echo "$cronjob"; } | crontab - >> $LOGFILE 2>&1
 ERROR=$?
