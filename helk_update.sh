7a055280f7d1f20bfcc3e4f56543dfc2d5790be9
Dev Dua <devdua@live.com>
2018-06-18 15:23:43 +0530

    Added option to update from Github + minor improvements

--- helk_update.sh
+++ helk_update.sh
@@ -12,6 +12,14 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+usage(){
+    echo "Usage: $0 [option...]" >&2
+    echo -e "\t-s\t\tSet update source"
+    echo -e "\t\t\t 1: Github (Stable, Recommended)"
+    echo -e "\t\t\t 2: Docker Hub"
+    exit 1
+}
+
 check_min_requirements(){
     systemKernel="$(uname -s)"
     echo "[HELK-UPDATE-INFO] HELK being hosted on a $systemKernel box"
@@ -20,10 +28,10 @@ check_min_requirements(){
         
         # Only checking Available Memory requirements and not Disk, as old images are deleted and replaced with updated ones.
         if [ "${AVAILABLE_MEMORY}" -ge "12" ] ; then
-            echo "[HELK-UPDATE-INFO] Available Memory: ${AVAILABLE_MEMORY}"
+            echo "[HELK-UPDATE-INFO] Available Memory (GB): ${AVAILABLE_MEMORY}"
         else
             echo "[HELK-UPDATE-ERROR] YOU DO NOT HAVE ENOUGH AVAILABLE MEMORY"
-            echo "[HELK-UPDATE-ERROR] Available Memory: ${AVAILABLE_MEMORY}"
+            echo "[HELK-UPDATE-ERROR] Available Memory (GB): ${AVAILABLE_MEMORY}"
             echo "[HELK-UPDATE-ERROR] Check the requirements section in our installation Wiki"
             echo "[HELK-UPDATE-ERROR] Installation Wiki: https://github.com/Cyb3rWard0g/HELK/wiki/Installation"
             mv docker-compose.yml.bak docker-compose.yml >> $LOGFILE 2>&1
@@ -32,6 +40,91 @@ check_min_requirements(){
         fi
     else
         echo "[HELK-UPDATE-INFO] Error retrieving memory info for $systemKernel. Make sure you have at least 12GB of available memory!"
+    fi
+}
+
+check_docker_hub(){
+    echo "[WARNING] Pulling updated images from Docker Hub may lead to an unstable deployment of the HELK, as it may not match the recommended & tested configuration posted in the Github repository."
+    if [ -x "$(command -v python)" ]; then
+        echo "Python is available" >> $LOGFILE
+    else
+        echo "Python is not available" >> $LOGFILE
+        apt-get -qq update >> $LOGFILE 2>&1 && apt-get -qqy install python >> $LOGFILE 2>&1
+        ERROR=$?
+        if [ $ERROR -ne 0 ]; then
+            echo "[!] Could not install Python (Error Code: $ERROR). Check $LOGFILE for details."
+            exit 1
+        fi
+        echo "Python installed." >> $LOGFILE
+    fi
+
+    for image in $HELK_IMAGES$EXTENSION_IMAGES
+    do
+        TAG=$(sudo docker images | grep $image | awk '{print $2}')
+        SIZE=$(sudo docker images | grep $image | awk '{print $7}')
+        echo -e "Local tag for $image --> $TAG | Size : $SIZE" >> $LOGFILE 2>&1
+        REMOTE_TAG="$(curl --silent "https://hub.docker.com/v2/repositories/$image/tags/" | python -c 'import sys, json; print (json.load(sys.stdin)["results"][0]["name"])')"    
+        
+        REMOTE_FULLSIZE="$(curl --silent "https://hub.docker.com/v2/repositories/$image/tags/" | python -c 'import sys, json; print (json.load(sys.stdin)["results"][0]["full_size"])')"
+        MB=$((1024*1024))
+        REMOTE_FULLSIZE_MB=$(($REMOTE_FULLSIZE/$MB))
+        echo -e "Remote tag for $image --> $REMOTE_TAG | Size : $REMOTE_FULLSIZE_MB MB\n" >> $LOGFILE 2>&1
+
+        [ "$TAG" = "$REMOTE_TAG" ] ; SHOULD_UPDATE=$?
+        if [ $SHOULD_UPDATE == "0" ]; then
+            echo "[+] $image is up to date"
+        else
+            REBUILD_NEEDED=1
+            docker rmi -f "$image:$TAG" >> $LOGFILE 2>&1
+            cp docker-compose.yml docker-compose.yml.bak
+            sed -i "s|$image:$TAG|$image:$REMOTE_TAG|" docker-compose.yml >> $LOGFILE 2>&1
+            ERROR=$?
+            if [ $ERROR -ne 0 ]; then
+                echo "[!] Could not update the HELK (Error Code: $ERROR). Check $LOGFILE for details."
+                echo "This could happen if 2 or more images for $image with different tags exist together. Please delete the image not being used by a running HELK container and try again."
+                mv docker-compose.yml.bak docker-compose.yml >> $LOGFILE 2>&1
+                rm docker-compose.yml.bak >> $LOGFILE 2>&1
+                exit 1
+            fi
+            echo -e "[+] Newer tag for $image found --> $REMOTE_TAG (current: $TAG) | Size --> $REMOTE_FULLSIZE_MB MB (current expanded size: $SIZE)."
+        fi
+    done
+}
+
+check_github(){
+    if [ -x "$(command -v git)" ]; then
+        echo -e "Git is available" >> $LOGFILE
+    else
+        echo "Git is not available" >> $LOGFILE
+        apt-get -qq update >> $LOGFILE 2>&1 && apt-get -qqy install git-core >> $LOGFILE 2>&1
+        ERROR=$?
+        if [ $ERROR -ne 0 ]; then
+            "[!] Could not install Git (Error Code: $ERROR). Check $LOGFILE for details."
+            exit 1
+        fi
+        echo "Git successfully installed." >> $LOGFILE
+    fi
+
+    if HELK_REPO_EXISTS=$(git remote | grep "helk-repo") && [ -z "$HELK_REPO_EXISTS" ]; then
+        git remote add helk-repo https://github.com/Cyb3rWard0g/HELK.git  >> $LOGFILE 2>&1 
+    else
+        echo "HELK repo exists" >> $LOGFILE 2>&1
+    fi
+
+    git checkout master >> $LOGFILE 2>&1
+
+    if PULL_NEEDED=$(git rev-list --left-right --count master...helk-repo/master | awk '{print $2}') && [ ! "$PULL_NEEDED" == "0" ]; then
+        echo "New release available. Commit diff --> $PULL_NEEDED" >> $LOGFILE 2>&1
+        echo "[HELK-UPDATE-INFO] New release available. Pulling new code."
+        git pull helk-repo master >> $LOGFILE 2>&1
+        ERROR=$?
+        if [ $ERROR -ne 0 ]; then
+            echo "[!] Could not run pull latest code (Error Code: $ERROR). Check $LOGFILE for details."
+            exit 1
+        fi
+        REBUILD_NEEDED=1
+    else
+        echo "[HELK-UPDATE-INFO] No updates available."    
     fi
 }
 
@@ -49,72 +142,53 @@ HELK_IMAGES=$(docker images --format "{{.Repository}}" | grep "cyb3rward0g/helk-
 # Extension images not hosted by cyb3rward0g
 EXTENSION_IMAGES=" thomaspatzke/helk-sigma"
 REBUILD_NEEDED=0
+SOURCES=("GITHUB" "DOCKER HUB")
+UPDATE_FUNCTIONS=(check_github check_docker_hub)
 
-echo -e "[HELK-UPDATE-INFO] CHECKING FOR UPDATES..."
-
-if [ -x "$(command -v python)" ]; then
-    echo "Python is available" >> $LOGFILE
-else
-    echo "Python is not available" >> $LOGFILE
-    apt-get -qq update >> $LOGFILE 2>&1 && apt-get -qqy install python >> $LOGFILE 2>&1
-    ERROR=$?
-    if [ $ERROR -ne 0 ]; then
-        echoerror "Could not install Python (Error Code: $ERROR)."
-        exit 1
-    fi
-    echo "Python installed." >> $LOGFILE
+if [ -z "$1" ]; then
+    usage
 fi
 
-for image in $HELK_IMAGES$EXTENSION_IMAGES
-do
-    TAG=$(sudo docker images | grep $image | awk '{print $2}')
-    SIZE=$(sudo docker images | grep $image | awk '{print $7}')
-    echo -e "Local tag for $image --> $TAG | Size : $SIZE" >> $LOGFILE 2>&1
-    REMOTE_TAG="$(curl --silent "https://hub.docker.com/v2/repositories/$image/tags/" | python -c 'import sys, json; print (json.load(sys.stdin)["results"][0]["name"])')"    
-    
-    REMOTE_FULLSIZE="$(curl --silent "https://hub.docker.com/v2/repositories/$image/tags/" | python -c 'import sys, json; print (json.load(sys.stdin)["results"][0]["full_size"])')"
-    MB=$((1024*1024))
-    REMOTE_FULLSIZE_MB=$(($REMOTE_FULLSIZE/$MB))
-    echo -e "Remote tag for $image --> $REMOTE_TAG | Size : $REMOTE_FULLSIZE_MB MB\n" >> $LOGFILE 2>&1
-
-    [ "$TAG" = "$REMOTE_TAG" ] ; SHOULD_UPDATE=$?
-    if [ $SHOULD_UPDATE == "0" ]; then
-        echo "[+] $image is up to date"
-    else
-        REBUILD_NEEDED=1
-        docker rmi -f "$image:$TAG" >> $LOGFILE 2>&1
-        cp docker-compose.yml docker-compose.yml.bak
-        sed -i "s|$image:$TAG|$image:$REMOTE_TAG|" docker-compose.yml >> $LOGFILE 2>&1
-        ERROR=$?
-        if [ $ERROR -ne 0 ]; then
-            echo "[!] Could not update the HELK (Error Code: $ERROR)."
-            echo "This could happen if 2 or more images for $image with different tags exist together. Please delete the image not being used by a running HELK container and try again."
-            mv docker-compose.yml.bak docker-compose.yml >> $LOGFILE 2>&1
-            rm docker-compose.yml.bak >> $LOGFILE 2>&1
-            exit 1
+while getopts ":s:h" opt; do
+  case $opt in
+    s)  if [ "$OPTARG" == "1" -o "$OPTARG" == "2" ]; then
+            echo "Chosen update source --> ${SOURCES[$OPTARG-1]}" >> $LOGFILE 2>&1
+            echo "[HELK-UPDATE-INFO] CHECKING ${SOURCES[$OPTARG-1]} FOR UPDATES."
+            ${UPDATE_FUNCTIONS[$OPTARG-1]}
+        else
+            echo "[!] Invalid update source option"
+            usage
         fi
-        echo -e "[+] Newer tag for $image found --> $REMOTE_TAG (current: $TAG) | Size --> $REMOTE_FULLSIZE_MB MB (current expanded size: $SIZE)."
-    fi
+        ;;
+    h)  usage
+        ;;
+    \?) echo "[!] Invalid option: -$OPTARG" >&2
+        usage
+        ;;
+    :)  echo "[!] Option -$OPTARG requires an argument." >&2
+        usage
+        ;;
+    esac
 done
 
 if [ $REBUILD_NEEDED == 1 ]; then
-    echo -e "[HELK-UPDATE-INFO] Stopping HELK and starting update..."
-    docker-compose kill >> $LOGFILE 2>&1
+    echo -e "[HELK-UPDATE-INFO] Stopping HELK and starting update"
+    docker-compose down >> $LOGFILE 2>&1
     check_min_requirements
 
     echo "[HELK-UPDATE-INFO] Rebuilding HELK via docker-compose"
     docker-compose up --build -d -V --force-recreate --always-recreate-deps >> $LOGFILE 2>&1
     ERROR=$?
     if [ $ERROR -ne 0 ]; then
-        echo "Could not run HELK via docker-compose (Error Code: $ERROR)."
+        echo "[!] Could not run HELK via docker-compose (Error Code: $ERROR). Check $LOGFILE for details."
         exit 1
     fi
 
-    get_jupyter_token
     sleep 180
-    echo -e "\n[HELK-UPDATE-INFO] YOUR HELK HAS BEEN UPDATED!"
+    get_jupyter_token
+    echo -e "[HELK-UPDATE-INFO] YOUR HELK HAS BEEN UPDATED!"
     rm docker-compose.yml.bak >> $LOGFILE 2>&1
 else
-    echo -e "\n[HELK-UPDATE-INFO] YOUR HELK IS ALREADY UP-TO-DATE."
+    echo -e "[HELK-UPDATE-INFO] YOUR HELK IS ALREADY UP-TO-DATE."
 fi
-chmod 666 docker-compose.yml
+chmod 666 docker-compose.yml  >> $LOGFILE 2>&1
