6c8b81b316fccb81040e99c8f7243076ee1a4fbf
nicholasaleks <nicholas.aleks@gmail.com>
2019-02-17 17:52:06 -0500

    Added user to the Jupyter spawner and notebook_dir

--- docker/helk-jupyter/jupyterhub/jupyterhub_config.py
+++ docker/helk-jupyter/jupyterhub/jupyterhub_config.py
@@ -3,14 +3,15 @@
 # Author: Roberto Rodriguez (@Cyb3rWard0g)
 # License: GPL-3.0
 
-#c = get_config()
+import getpass
+user = getpass.getuser()
 
 c.JupyterHub.log_level = 10
 
 c.Authenticator.admin_users = {'helk'}
 c.Spawner.cmd = ['jupyter-labhub']
-c.Spawner.notebook_dir = '/opt/helk/jupyterhub'
-
+c.Spawner.notebook_dir = '/opt/helk/jupyterhub/{user}'.format(user=user)
+c.NotebookApp.notebook_dir = '/opt/helk/jupyterhub/{user}'.format(user=user)
 c.JupyterHub.hub_ip = 'helk-jupyter'
 c.JupyterHub.port = 8000
 c.JupyterHub.base_url = '/jupyter'
