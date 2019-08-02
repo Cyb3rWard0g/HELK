df8e4598fbdd02f3958310f79364e61ab241c79a
neu5ron <therealneu5ron@gmail.com>
2019-08-02 14:57:00 -0400

    - G1GC tweaks - better export settings if user has custom and we can still apply HELKs tunings

--- docker/helk-logstash/scripts/logstash-entrypoint.sh
+++ docker/helk-logstash/scripts/logstash-entrypoint.sh
@@ -144,29 +144,25 @@ if [[ -z "$LS_JAVA_OPTS" ]]; then
     # Check using more accurate MB
     AVAILABLE_MEMORY=$(awk '/MemAvailable/{printf "%.f", $2/1024}' /proc/meminfo)
     if [ $AVAILABLE_MEMORY -ge 900 -a $AVAILABLE_MEMORY -le 1000 ]; then
-      LS_MEMORY=400
-      LS_MEMORY_HIGH=1000
-      export LS_JAVA_OPTS="-Xms${LS_MEMORY}m -Xmx${LS_MEMORY_HIGH}m"
-      break
+      LS_MEMORY="400m"
+      LS_MEMORY_HIGH="1000m"
     elif [ $AVAILABLE_MEMORY -ge 1001 -a $AVAILABLE_MEMORY -le 3000 ]; then
-      LS_MEMORY=700
-      LS_MEMORY_HIGH=1300
-      export LS_JAVA_OPTS="-Xms${LS_MEMORY}m -Xmx${LS_MEMORY_HIGH}m"
-      break
+      LS_MEMORY="700m"
+      LS_MEMORY_HIGH="1300m"
     elif [ $AVAILABLE_MEMORY -gt 3000 ]; then
       # Set high & low, so logstash doesn't use everything unnecessarily, it will usually flux up and down in usage -- and doesn't "severely" despite what everyone seems to believe
       LS_MEMORY=$(( AVAILABLE_MEMORY / 4 ))
       LS_MEMORY_HIGH=$(( AVAILABLE_MEMORY / 2 ))
       if [ $AVAILABLE_MEMORY -gt 31000 ]; then
-        LS_MEMORY=8000
-        LS_MEMORY_HIGH=31000
+        LS_MEMORY="8000m"
+        LS_MEMORY_HIGH="31000m"
       fi
-      export LS_JAVA_OPTS="-Xms${LS_MEMORY}m -Xmx${LS_MEMORY_HIGH}m"
-      break
     else
       echo "[HELK-LOGSTASH-DOCKER-INSTALLATION-INFO] $LS_MEMORY MB is not enough memory for Logstash yet.."
       sleep 1
     fi
+    export LS_JAVA_OPTS="${HELK_LOGSTASH_JAVA_OPTS} -Xms${LS_MEMORY} -Xmx${LS_MEMORY_HIGH} "
+    break
   done
 fi
 echo "[HELK-LOGSTASH-DOCKER-INSTALLATION-INFO] Setting LS_JAVA_OPTS to $LS_JAVA_OPTS"
